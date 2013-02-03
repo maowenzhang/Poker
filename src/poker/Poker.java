300f98420a27a2dc10b3f7d3baacca98e58317c4
Jonathan Lifschutz <jonathan.lifschutz@cantab.net>
2013-02-03 14:21:28 +0000

    initial commit

---
+++ src/poker/Poker.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poker;
+
+/**
+ *
+ * @author Jonathan
+ */
+import java.util.Scanner;
+
+public class Poker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            round();
+            System.out.println("Do you want to continue playing?");
+        } while (!scan.next().equals("no"));
+
+    }
+
+    public static void round() {
+
+        dealCards();
+
+        evaluateHands();
+
+        displayResults();
+
+    }
+
+    private static void dealCards() {
+    }
+
+    private static void evaluateHands() {
+    }
+
+    private static void displayResults() {
+    }
+}
