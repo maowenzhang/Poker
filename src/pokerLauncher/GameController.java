71a947b61fe3e73e6f28c87326f9cdda21801520
Tom Hedges <tom@tomhedges.com>
2013-03-30 21:08:26 +0000

    re-commit to add unstaged file

---
+++ src/pokerLauncher/GameController.java
@@ -0,0 +1,68 @@
+package pokerLauncher;
+
+import java.util.Observable;
+
+public class GameController extends Observable {
+	
+	private int playerScore = 0;
+	private int dealerScore = 0;
+	
+	private int roundNumber = 0;
+	
+	boolean isPlayerTurn = true;
+	
+	boolean dealHands = false;
+	boolean exchangeCards = false;
+	boolean showDealerHand = false;
+	boolean scoreHands = false;
+	
+
+	public void setDealHands(){
+		
+		dealHands = true;
+		
+	}
+	
+	public void setexchangeCards(){
+		
+		exchangeCards = true;
+		
+	}
+	
+	public void setShowDealerHand(){
+		
+		showDealerHand = true;
+		
+	}
+	
+	public void setScoreHands(){
+		
+		scoreHands = true;
+		setChanged();
+		notifyObservers(new Boolean(this.scoreHands));
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * public void setPlayerScore(int playerScore) {
+	
+		this.playerScore = this.playerScore + playerScore;
+		notifyObservers(this.playerScore);	
+	}
+
+
+	public int getPlayerScore() {
+		return playerScore;
+	}
+	
+	 */
+}
